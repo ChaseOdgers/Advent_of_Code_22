aa405ee49688c0dfa7db230dc38d7cd1851d2f7c
chasewodgers <chase.odgers@dialexa.com>
2022-12-03 15:25:06 -0600

    Day3 Part2 Done

--- Day_3/day3AoC.cpp
+++ Day_3/day3AoC.cpp
@@ -1,66 +1,67 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <chrono>
+#include <vector>
 using namespace std::chrono;
+
 
 int main(){
 
-    //Timer Start
-    // auto start = high_resolution_clock::now();
+    //Start run timer
+    auto start = high_resolution_clock::now();
 
-    //Initiate file io
+    //Initiate io
     std::ifstream inFile;
     inFile.open("day3_input.txt");
-    std::ofstream outFile;
-    outFile.open("day3_output.txt");
 
-    //Declare vars
-    int total=0, h1=0,h2=0;
-    std::string line, half1, half2;
-    std::vector<char> vecHalf1, vecAns;
+    //Declare variables
+    int total=0, total2=0, linecount= 0, groupCase=0;
+    std::string line, half1, half2, groupOfElves[2];
+    std::vector<char> commonLetter;
 
-/*
-    - Problem statement
-    - Rucksack has 2 compartments
-    - Each item maps to 1 of the compartments
-    - 1 misplaced item per rucksack
-    - Each rucksack is 1 line, each char on line is 1 item
-    - 1st half of line = compartment 1 second half is compartment 2
-    - Lowercase = 1-26
-    - Uppercase = 27-52
-    
-    Q1: what item appears in both compartments
-*/
 
-    //Read input line by line
+    //Find letters in string that are in both compartments, add values to total
     while (std::getline(inFile, line)){
+
+        //Part 1
         half1 = line.substr(0,line.size()/2);
         half2 = line.substr(line.size()/2,line.size());
         for(auto i:half2){
             if(half1.find_first_of(i) != std::string::npos){
-                vecAns.push_back(i);
+                if(int(i)-96 <0){ total+=int(i)-38; }
+                else{ total+=int(i)-96; }
                 break;
             }
         }
-    }
-    for(auto i:vecAns){
-        if(int(i)-96 <0){
-            std::cout<<"Letter: "<<i<<" Dec:"<<int(i)-64+26<<std::endl;
 
-            total+=int(i)-64+26;
-            //abs(i)
+        //Part 2
+        if(linecount%3 < 2){ groupOfElves[linecount%3] = line; }
+
+        if(linecount%3 == 2){
+            for(auto letter:groupOfElves[0]){
+                //Add the common letter between 2 first strings to vector 
+                if(groupOfElves[1].find_first_of(letter) != std::string::npos){
+                    commonLetter.push_back(letter);
+                }
+            }
+
+            //Check vector of letters common between 1st two strings against the third
+            for(auto letter:commonLetter){
+                if(line.find_first_of(letter) != std::string::npos){
+                    if(int(letter)-96 <0){ total2+=int(letter)-38; }
+                    else{ total2+=int(letter)-96; }
+                    commonLetter.clear();
+                    break;
+                }
+            }
         }
-        else{
-            std::cout<<"Letter: "<<i<<" Dec:"<<int(i)-96<<std::endl;
-            total+=int(i)-96;
-        }
+        linecount++;
     }
 
- std::string alphabet = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    for(int i=0; i<=52; i++){
-        outFile<<i<<" "<<alphabet[i]<<"\n";
-    }
+    std::cout<<"Total: "<<total<<std::endl;
+    std::cout<<"Total 2: "<<total2<<std::endl;
 
-std::cout<<"Total: "<<total<<std::endl;
+    auto stop = high_resolution_clock::now();
+    auto duration1 = duration_cast<microseconds>(stop - start);
+    std::cout<<"\nRuntime:\n."<<duration1.count()<<" milliseconds\n";
 }
